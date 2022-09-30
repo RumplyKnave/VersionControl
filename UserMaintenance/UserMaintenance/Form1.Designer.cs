70c0694b24ad62c66343f4a96461758c4b29d4e9
RumplyKnave <nadasdipeter2000@gmail.com>
2022-09-30 10:27:23 +0200

    Harmadik commit

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -47,17 +47,18 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(397, 163);
+            this.button1.Location = new System.Drawing.Point(379, 128);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(228, 33);
+            this.button1.Size = new System.Drawing.Size(260, 33);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Hozzáadás";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(394, 48);
+            this.label1.Location = new System.Drawing.Point(376, 48);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 16);
             this.label1.TabIndex = 3;
@@ -66,7 +67,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(394, 102);
+            this.label2.Location = new System.Drawing.Point(376, 86);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 4;
@@ -74,16 +75,16 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(525, 48);
+            this.textBox1.Location = new System.Drawing.Point(503, 45);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(136, 22);
             this.textBox1.TabIndex = 5;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(525, 96);
+            this.textBox2.Location = new System.Drawing.Point(503, 83);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.Size = new System.Drawing.Size(136, 22);
             this.textBox2.TabIndex = 6;
             // 
             // Form1
