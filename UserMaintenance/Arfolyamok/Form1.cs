b666723d7b4a7022acb67cbd01e11eff333e4009
RumplyKnave <nadasdipeter2000@gmail.com>
2022-10-21 10:39:57 +0200

    XML

--- UserMaintenance/Arfolyamok/Form1.cs
+++ UserMaintenance/Arfolyamok/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Arfolyamok
 {
@@ -17,14 +18,39 @@ namespace Arfolyamok
         public Form1()
         {
             InitializeComponent();
-            loading();
             dataGridView1.DataSource = Rates;
+            XMLedit(loading());
         }
-        
+
+        private void XMLedit(string result)
+        {
+            
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+        }
+
         BindingList <RateData> Rates = new BindingList<RateData> ();
         
         
-        private void loading()
+        private string loading()
         {
 
             var mnbService = new MNBArfolyamServiceSoapClient();
@@ -40,7 +66,7 @@ namespace Arfolyamok
 
            var result = response.GetExchangeRatesResult;
 
-            
+            return result;
         }
     }
 
