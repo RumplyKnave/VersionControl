4c6f8d2accf2a0447c3ec7c69508f9b5fbe4aa13
RumplyKnave <nadasdipeter2000@gmail.com>
2022-11-04 10:38:05 +0100

    Színgomb

--- UserMaintenance/Xmas/Form1.cs
+++ UserMaintenance/Xmas/Form1.cs
@@ -80,5 +80,16 @@ namespace Xmas
         {
             Factory = new BallFactory();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
+        }
     }
 }
